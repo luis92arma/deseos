b13b77ebdc4b97e65adce6572f8374c072f8f619
luis92arma <luis.arma92@gmail.com>
2020-06-27 15:32:59 -0500

    alert controller

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -11,13 +12,24 @@ export class Tab1Page {
 
   constructor(
     public deseosService: DeseosService,
-    private router: Router
+    private router: Router,
+    public alertCtrl: AlertController
   ) {
 
   }
 
-  agregarLista(){
-    this.router.navigateByUrl('/tabs/tab1/agregar')
+  async agregarLista(){
+    // funcion async funciona como una promesa
+    //this.router.navigateByUrl('/tabs/tab1/agregar')
+    const alert = await this.alertCtrl.create({
+      header: 'Alert',
+      subHeader: 'Subtitolo',
+      message: 'Soy una alerta prro',
+      buttons: ['OK']
+    });
+
+    alert.present();
+
   }
 
 }
